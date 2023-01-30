863339949efe7776439eb1e3f57ed7771b0513ce
mahmoudv2017 <mahmoud.elbermbaly2017@yahoo.com>
2023-01-30 23:22:11 +0200

    asd

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://testebdev592049847296-env.eba-rktip3ym.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
